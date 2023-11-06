226b6eef77be0441b3602e08e9c969a8684d03f4
Joseph Baruch <josephbaruch48@gmail.com>
2023-11-06 11:02:53 -0800

    Finishing off the code for Program One

--- ProjectOne/ProjectOne.py
+++ ProjectOne/ProjectOne.py
@@ -13,26 +13,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+"""
+    ------ HTTP Request to Alpha Vantage Stock Price API -----
+"""
 # ---- HTTP Request to API  for data ----
 url = 'https://www.alphavantage.co/query?'
-key2 = '&apikey=W8UBPRCPRPG0F5PJ'
- # key = '&apikey=39G28MAGU76LGPYW'
+key = '&apikey=39G28MAGU76LGPYW'
 function = 'function=TIME_SERIES_MONTHLY'
 symbol = '&symbol=IBM' # Specifies the stock for IBM Company
-# IBM 
-totalURL = url + function + symbol + key2 # Preparing string query URL 
-data = requests.get( totalURL ).json() #
+totalURL = url + function + symbol + key # Preparing string query URL 
+data = requests.get( totalURL ).json() # HTTP GET request 
 
-data_in_df = pd.DataFrame(data["Monthly Time Series"]) # converting to dataframe
-
+"""
+    ----- Pandas DataFrame -----
+"""
+ # converting to dataframe and entering correct sub-category
+data_in_df = pd.DataFrame(data["Monthly Time Series"])
 IBM_data = data_in_df.transpose() # switching X and Y for proper format
 
-IBM_data['index'] = range(1, len(IBM_data) + 1) # creating new column for index's
+"""
+    ----- Data Manipulation using numpy and other -----
+"""
+# fetch stock high and low values and convert to array
+high_val = IBM_data["2. high"].to_numpy() 
+low_val = IBM_data["3. low"].to_numpy()
 
-open_val = IBM_data["2. high"].to_numpy() # Convert to array
-#open_val = np.flip(open_val) # flip values in the array so they are old -> new
-
-# Convert the array of strings into an array of floats for better plotting in matplotlib
+# Convert the array of strings into an array of floats for matplotlib
 def convert_strings_to_floats(input_array):
     output_array = []
     for element in input_array:
@@ -40,22 +46,29 @@ def convert_strings_to_floats(input_array):
         output_array.append(converted_float)
     return output_array
 
-# Convert plotted values into floats for easier and cleaner plotting
-values_as_float = convert_strings_to_floats(open_val)
-indeces = convert_strings_to_floats(IBM_data['index'])
+# Call above function for high and low values
+high = convert_strings_to_floats(high_val)
+low = convert_strings_to_floats(low_val)
 
-# Plot the open values for IBM using matplotlib 
+"""
+    ------ MatPlotLib --------
+""" 
 plt.style.use('_mpl-gallery')
 
 # plot
-
 fig, ax = plt.subplots()
-ax.plot(pd.to_datetime(IBM_data.index.values), values_as_float, linewidth=1.0)
 
-plt.title("IBM Open Price (2000-2024)")
+# Two seperate plots (high and low values)
+ax.plot(pd.to_datetime(IBM_data.index.values), high, linewidth=1.0, color="red", label="high")
+ax.plot(pd.to_datetime(IBM_data.index.values), low, linewidth=1.0, color="green", label="low")
+
+# Labeling
+plt.title("IBM High and Low Price (2000-2024)")
 plt.xlabel("Time (year)")
 plt.ylabel("Cost ( US Dollars )")
+plt.legend(loc="upper left")
 
+# Rotating x-axis labels for better viewing
 plt.xticks(rotation=45, ha='right')
 
 plt.show()     
